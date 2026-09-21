882ef37780ac44b846d2ecf2d8df463b46518946
Alexx225 <jesalehs2005@gmail.com>
2026-09-20 23:01:42 -0600

    chore: configuracion inicial de Navigation y RecyclerView para Practica 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.recyclerview)
 }
