2d763c37e62e4b86f31070184105907b81bf9d0b
Alexx225 <jesalehs2005@gmail.com>
2026-09-23 00:25:55 -0600

    Cambios conjuntos funcionando

--- app/src/main/java/com/example/cityreport/ListaIncidenciasFragment.kt
+++ app/src/main/java/com/example/cityreport/ListaIncidenciasFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cityreport.databinding.FragmentListaIncidenciasBinding
+import com.example.cityreport.model.Incidencia
 
 class ListaIncidenciasFragment : Fragment() {
 
@@ -25,5 +26,20 @@ class ListaIncidenciasFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null // Previene fugas de memoria (memory leaks)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 1. Crear datos de prueba usando el modelo Incidencia (Persona A)
+        val listaMock = listOf(
+            Incidencia(1, "Bache en Av. Principal", "Bache profundo causa tráfico.", "Av. Central #102", "Pendiente"),
+            Incidencia(2, "Fuga de Agua", "Tubería rota en banqueta.", "Calle Hidalgo #45", "En proceso"),
+            Incidencia(3, "Luminaria Fundida", "Falta de alumbrado público.", "Parque Lineal", "Resuelto")
+        )
+
+        // 2. Conectar el RecyclerView con el Adapter (Persona A + Persona B)
+        binding.rvIncidencias.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(requireContext())
+        binding.rvIncidencias.adapter = IncidenciaAdapter(listaMock)
     }
 }
